9ad89a1fed46f5d6b0edc9e19dddbc7c4f666ce6
Dmitry Smirnov <patrickjsmirnov@gmail.com>
2024-07-26 13:11:33 +0200

    add external link icon

--- src/components/ResumeJobsList/ResumeJobsListItem/ResumeJobsListItem.tsx
+++ src/components/ResumeJobsList/ResumeJobsListItem/ResumeJobsListItem.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import Image from "next/image";
 
 import { ResumeJob } from "@/app/types";
 
@@ -24,9 +25,19 @@ export const ResumeJobsListItem: FC<Props> = (props) => {
   return (
     <li className={className}>
       <h2 className="text-2xl font-semibold mb-2">{position}</h2>
-      <div className="mb-2">
-        <a target="_blank" className="font-semibold" href={companyLink}>
+      <div className="flex flex-wrap gap-1 mb-2">
+        <a
+          target="_blank"
+          className=" flex gap-1 font-semibold"
+          href={companyLink}
+        >
           {companyName}
+          <Image
+            src="./external-link.svg"
+            width={18}
+            height={18}
+            alt="external link icon"
+          />
         </a>{" "}
         — <span className="italic">{period}</span>
       </div>
